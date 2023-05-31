c2f7537e666046f9729db6fd02feaaad4e81c9e2
Tarislada <57513585+Tarislada@users.noreply.github.com>
2023-05-31 13:54:09 +0900

    added clustering

--- python_practice/movielensanalysis.py
+++ python_practice/movielensanalysis.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -6,7 +7,17 @@ import plotly.express as px
 from scipy.spatial.distance import pdist, dice
 from sklearn.metrics import pairwise_distances
 import gower
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import KMeans
+from sklearn.cluster import SpectralClustering, OPTICS
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram
+import hdbscan
+import prince
+import altair as alt
+import altair_viewer as alt_viewer
+import plotly.express as px
+from sklearn import metrics as mt
+
 
 search = SearchEngine()
 
@@ -194,15 +205,89 @@ users['Region'] = users['States'].map(state_to_region)
 new_users = users.drop(columns=['zipcode','zipcode_simple','States'])
 new_users['occupation']=pd.Categorical(new_users.occupation)
 users_encoded = pd.get_dummies(new_users)
+users_encoded_noid = users_encoded.drop('user_id',axis=1)
 
+# Creating distance matrix of user data: Gower's, Dice
 dice_dist = pairwise_distances(users_encoded.values, metric='dice')
 float_cols = users_encoded.select_dtypes(include=[np.int]).columns
 users_encoded[float_cols] = users_encoded[float_cols].astype(np.float64)
 gower_dist = gower.gower_matrix(users_encoded.values)
-labels = SpectralClustering(n_clusters=5, affinity='precomputed').fit_predict(gower_dist)
 
-# Distance matrix - Dice or Gowers
-# K-medoids, hieracrchical, spectral clustering
+# Clustering using user distance matrices
+opticlabels = OPTICS(metric='precomputed').fit_predict(dice_dist)
+hdbscanlabels = hdbscan.HDBSCAN(metric='precomputed').fit_predict(dice_dist)
+
+go_opticlabels = OPTICS(metric='precomputed').fit_predict(gower_dist)
+go_hdbscanlabels = hdbscan.HDBSCAN(metric='precomputed').fit_predict(gower_dist)
+
+# dimension reduction on user data
+mca = prince.MCA(n_components=3,n_iter=100,random_state=101)
+usersmca = mca.fit(users_encoded_noid)
+users_mca = usersmca.transform(users_encoded_noid)
+usersmca.eigenvalues_summary
+alt.data_transformers.disable_max_rows()
+mca.row_coordinates(users_encoded_noid)
+fig = px.scatter_3d(users_encoded_noid, x=0, y=1, z=2)
+fig.show()
+
+contribution = usersmca.column_contributions_
+
+# Clustering again with dr'ed user data
+mca_optic = OPTICS(metric='mahalanobis').fit_predict()
+mca_hdbscan =  hdbscan.HDBSCAN(metric='mahalanobis').fit_predict()
+
+# Preparing Movie information for calculation
+genre_dummy = pd.get_dummies(pd.DataFrame(movies['genre'].tolist()).stack()).sum(level=0)
+new_movies = pd.concat([movies[['movie_id','title','decade']],genre_dummy],axis=1)
+movie_encoded = new_movies.drop(['movie_id','title'],axis=1)
+
+moviesmca = mca.fit(movie_encoded)
+moviesmca.eigenvalues_summary
+movies_mca = moviesmca.transform(movie_encoded)
+fig = px.scatter_3d(mca.row_coordinates(movie_encoded))
+fig.show()
+
+
+
+
+clustermca = prince.MCA(n_components=10,n_iter=100,random_state=101)
+
+meansil = np.zeros((19,10))
+clusteruserdata = clustermca.fit_transform(users_encoded_noid)
+for iter in range(10):
+    for ii in range(2,21):
+        kmeanscluster = KMeans(n_clusters=ii)
+        meansil[ii-2,iter] = mt.silhouette_score(clusteruserdata, kmeanscluster.fit_predict(clusteruserdata))
+meanmeansil = np.mean(meansil,axis=1)
+semmeansil = np.std(meansil,axis=1) / np.sqrt(meansil.shape[1])
+fig, ax = plt.subplots()
+ax.errorbar(np.arange(meansil.shape[0]), meanmeansil, yerr=semmeansil, fmt='o', capsize=5,ls='-')
+plt.show()
+
+
+moviemeansil = np.zeros((19,10))
+clustermoviedata = clustermca.fit_transform(movie_encoded)
+for iter in range(10):
+    for ii in range(2,21):
+        kmeanscluster = KMeans(n_clusters=ii)
+        moviemeansil[ii-2,iter] = mt.silhouette_score(clustermoviedata, kmeanscluster.fit_predict(clustermoviedata))
+moviemeanmeansil = np.mean(moviemeansil,axis=1)
+moviesemmeansil = np.std(moviemeansil,axis=1) / np.sqrt(moviemeansil.shape[1])
+fig, ax = plt.subplots()
+ax.errorbar(np.arange(moviemeansil.shape[0]), moviemeanmeansil, yerr=moviesemmeansil, fmt='o', capsize=5,ls='-')
+plt.show()
+
+# the results of this shows that k-12 students are special cases - 93% contribution on 1st axis
+# the second axis is also made up of occ 19 97% - unemployed
+# third axis is ambiguous - 31% max, farmers contributing 31% of the axis
+
+
+
+#labels = SpectralClustering(n_clusters=5, affinity='precomputed',verbose=True,n_jobs=4, eigen_solver='amg',assign_labels='cluster_qr').fit_predict(gower_dist)
+
+
+# Distance matrix - Dice or Gowers V
+# K-medoids, hieracrchical, spectral clustering, hdbscan, optic
 
 # plan for genre - 18 binaries or 4 genre variables that contain 1st~4th genre of the movie V
 # add statecode or division based on zipcode and http://www.structnet.com/instructions/zip_min_max_by_state.html V
